804f6a250b109b0cb0349ef2cba8c4b1c5e34c55
Oliver Liongson <44100716+olsantiago@users.noreply.github.com>
2025-10-03 17:41:43 -0700

    Update map.js

--- src/components/section-components/map.js
+++ src/components/section-components/map.js
@@ -4,7 +4,7 @@ class Map extends Component {
     return (
       <div className="google-map mb-120">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3262.832517163926!2d-118.31349!3d33.72302!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c63e0xxxxxx!2s2515%20S%20Western%20Ave%2C%20San%20Pedro%2C%20CA%2090732!5e0!3m2!1sen!2sus!4v1699999999999"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3305.990878906215!2d-118.47080852428391!3d34.04410507316007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2bb5c9a15964f%3A0x203fdd7cef5b840b!2s12121%20Wilshire%20Blvd%20%231201%2C%20Los%20Angeles%2C%20CA%2090025!5e0!3m2!1sen!2sus!4v1759538453859!5m2!1sen!2sus"
           width="100%"
           height="100%"
           frameBorder={0}
